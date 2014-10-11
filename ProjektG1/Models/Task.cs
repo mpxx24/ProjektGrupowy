8fb9457954c25af491c1ad3463a0153ecfdaeb56
mpxx24 <mpxx24@gmail.com>
2014-10-12 00:30:31 +0200

    podstawowa klasa

---
+++ ProjektG1/Models/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjektG1.Models
+{
+    public class Task
+    {
+        public string Tytul { get; set; }
+        public string OsobaOdpowiedzialna { get; set; }
+        public DateTime DataDodania { get; set; }
+        public DateTime Termin { get; set; }
+        public string Komentarz { get; set; }
+    }
+}
