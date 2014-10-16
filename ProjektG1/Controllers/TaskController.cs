31699d37fe622a933e717f3c073a284bc7de0efb
mpxx24 <mpxx24@gmail.com>
2014-10-16 20:07:51 +0200

    działa dodawanie (nie jest ładne, no ale cóż...)

--- ProjektG1/Controllers/TaskController.cs
+++ ProjektG1/Controllers/TaskController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,21 +12,60 @@ namespace ProjektG1.Controllers
     public class TaskController : Controller
     {
         
-        public ViewResult Zadanie()
+        public ActionResult Zadanie()
         {
             var taskContext = new TaskContext();
-            var ListaTaskow = new List<Task>();
+            var listaTaskow = new List<Task>();
             //Task task = taskContext.Tasks.Single(tk => tk.Tytul =="Zadanie");
             //var task = taskContext.Tasks.Single(tk => tk.ID == taskId);
 
             foreach (var task in taskContext.Tasks)
             {
-                ListaTaskow.Add(task);
+                listaTaskow.Add(task);
             }
 
-            ViewBag.Zadanie = ListaTaskow;
+            ViewBag.Zadanie = listaTaskow;
 
             return View();
+        }
+
+        ////public ActionResult DodajTask(string tytul, string osobaOdp, DateTime termin, string komentarz)
+        ////{
+        ////    var context = new TaskContext();
+        ////    var nowyTask = new Task()
+        ////    {
+        ////        Tytul = tytul,
+        ////        DataDodania = DateTime.Now,
+        ////        OsobaOdpowiedzialna = osobaOdp,
+        ////        Termin = termin,
+        ////        Komentarz = komentarz
+        ////    };
+        ////    context.Tasks.Add(nowyTask);
+        ////    context.SaveChanges();
+        ////}
+        
+        
+        public ActionResult DodajTask()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create()
+        {
+            var taskContext = new TaskContext();
+            var noweZadanie = new Task()
+            {
+                Tytul = Request["Tytul"],
+                OsobaOdpowiedzialna = Request["OsobaOdpowiedzialna"],
+                Komentarz = Request["Komentarz"],
+                DataDodania = DateTime.Now,
+                Termin = DateTime.Today
+            };
+            taskContext.Tasks.Add(noweZadanie);
+            taskContext.SaveChanges();
+            return RedirectToAction("Zadanie", "Task");
         }
 
     }
