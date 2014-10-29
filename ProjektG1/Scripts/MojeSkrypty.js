47d15b53f6aef85d089a2ae819de8509f1d77e16
mpxx24 <mpxx24@gmail.com>
2014-10-29 12:13:48 +0100

    plik z własnymi skryptami

---
+++ ProjektG1/Scripts/MojeSkrypty.js
@@ -0,0 +1,21 @@
+﻿//jQuery scripts - mpxx24
+
+$(function () {
+
+    //datapicker 
+    $('.date-picker').datepicker();
+
+    //drag and drop
+    $(".ui-widget-content").draggable({ revert: "invalid" });
+    $(".ui-widget-header").droppable();
+
+    //do not show toggler on page load
+    $(document).ready(function () {
+        $(".toggler").toggle(false);
+    });
+
+    //show toggle on click
+    $("#ButtonTask").click(function () {
+        $(".toggler").toggle("blind");
+    });
+});
