dcd92e45f5312ec4d21d347403721a7fec646519
mpxx24 <mpxx24@gmail.com>
2014-11-25 13:09:14 +0100

    poprzednia wersja DodajTask()

--- ProjektG1/Scripts/MojeSkrypty.js
+++ ProjektG1/Scripts/MojeSkrypty.js
@@ -1,6 +1,6 @@
 ﻿$(function () {
 
-    $(".date-picker").datepicker({minDate: 0});
+    $(".date-picker").datepicker({ minDate: 0 });
 
     $(".task").draggable({ revert: "invalid" });
     $(".task-group").droppable();
@@ -12,11 +12,12 @@
     $(".button-task-text").click(function () {
         $(this).siblings(".task-info").toggle("blind");
     });
-    
+
     $(".findParentsId").click(function () {
-        var mDiv = $(this).parents("div").slice(1,2).attr("title");
+        var mDiv = $(this).parents("div").slice(1, 2).attr("title");
         $(function () {
             sessionStorage.GroupId = mDiv;
+            $(".jatopierdole").attr("value", mDiv);
         });
     });
 
@@ -24,17 +25,27 @@
         $("#TaskGroupIdRetriever").attr("value", sessionStorage.GroupId);
     });
 
-    $(function() {
+    $(function () {
         $("[class='button-task-text'][title='True']").css("text-decoration", "line-through");
     });
 
-    $("#AddTaskButton").click(function() {
+    $(".findParentsId").click(function () {
         $("#dodajTask").show();
     });
 
     $("#CreateTaskButton").click(function () {
         $("#dodajTask").hide();
     });
+
+    //$(function () {
+    //    $(".task").mousedown(function () {
+    //        $(".task-background").css("background-color", "grey").css("border", "1px dashed rgba(0,0,0,.2)");
+    //    });
+    //    $(".task").mouseup(function () {
+    //        $(".task-background").css("background-color", "white").css("border", "none");
+    //    });
+
+    //});
 });
 
 
